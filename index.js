e49ce23f7e5b895d95af4c59482748137b0cb048
Abul-Kashem <kashem.mcpd@gmail.com>
2022-10-29 16:22:57 +0600

    Manually CRUD file created and tested

--- index.js
+++ index.js
@@ -8,21 +8,50 @@
 // dependencies
 const http = require('http')
 const { handleReqRes } = require('./helpers/handleReqRes')
+const environment = require('./helpers/environments')
+const data = require('./lib/data')
 
 // app object - module scaffolding
 const app = {}
 
+
+// tesing file system
+// data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
+//     console.log(`error was`, err)
+// })
+
+// data.read('test', 'newFile', (err, result) => {
+//     console.log(err, result)
+// })
+
+// data.update('test', 'newFile', { name: 'England', language: 'English' }, (err) => {
+//     console.log(`error was`, err)
+// })
+
+// data.delete('test', 'newFIle', (err) => {
+//     console.log(err)
+// })
+
+
+
+
 // configuration
-app.config = {
-    port: 3000,
-}
+// app.config = {
+//     port: 3000,
+// }
+
 
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes)
-    server.listen(app.config.port, () => {
-        console.log(`environment variable is ${process.env.NODE_ENV}`)
-        console.log(`listening to port ${app.config.port}`)
+    // server.listen(app.config.port, () => {
+    //     // console.log(`environment variable is ${process.env.NODE_ENV}`)
+    //     console.log(`listening to port ${app.config.port}`)
+    // })
+
+    server.listen(environment.port, () => {
+        // console.log(`environment variable is ${process.env.NODE_ENV}`)
+        console.log(`listening to port ${environment.port}`)
     })
 }
 
